07a6126cdbcaedb12453c745e0bc83b893ead936
Thniga <jeannaxx2546@gmail.com>
2026-02-28 23:14:00 +0700

    fihisss

--- Week6/app/(tabs)/_layout.tsx
+++ Week6/app/(tabs)/_layout.tsx
@@ -40,6 +40,8 @@ export default function TabLayout() {
                     ),
                 }}
             />
+            
+            
             <Tabs.Screen
                 name="profile"
                 options={{
