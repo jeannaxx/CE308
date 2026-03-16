6cc8bbc404030bd5aaee90a9433718d75f430631
Thniga <jeannaxx2546@gmail.com>
2026-03-16 09:44:27 +0700

    Week8

--- Week8/backend/src/index.ts
+++ Week8/backend/src/index.ts
@@ -1,15 +1,56 @@
-import express from 'express';
+// import express from 'express';
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+
+// app.use(express.json());
+
+// app.get('/',(req,res) => {
+//     res.send('Hello from Prisma AP!');
+// });
+
+// app.listen(port,() =>{
+//    console.log(`Sever is runing on http://localhost:${port}`);
+    
+// });
+
+import  express  from "express";
+import {prisma} from '../lib/prisma'
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.get('/',(req,res) => {
-    res.send('Hello from Prisma AP!');
+//create book
+app.post('api/book',async(req,res) => {
+    const{title,author,description,price} = req.body;
+    try{
+        const book = await prisma.book.create({
+            data:{
+                title,
+                author,
+                description,
+                price,
+            },
+        });
+        res.status(201).json(book);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({error:'An eroor occueewd while creating the book'});
+    }
 });
-
-app.listen(port,() =>{
-   console.log(`Sever is runing on http://localhost:${port}`);
-    
-});
+app.get('api/books/:id',async(req,res)=>{
+    const{id} = req.params;
+    try{
+        const book = await prisma.book.findUnique({
+            where:{bookId:id},
+        });
+        if(book){
+            res.json(book);
+        }else{
+            res.status(404).json({error:'Book not found.'});
+        }
+    }
+})
---
+++ Week8/backend_b/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('Hello from Prisma AP!');
+});
+
+app.listen(port,() =>{
+   console.log(`Sever is runing on http://localhost:${port}`);
+    
+});
